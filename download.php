ca50e0f4b7e22fdc0b99d0d1720b4c142a27ccaa
gautier.keitaro <keitarohd@gmail.com>
2018-03-14 15:29:03 +0100

    TransV3

---
+++ download.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <script src="js/background.js" async></script>
+    <script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+    <title>Document</title>
+</head>
+
+<header class="menu">
+
+<button><a class="a1">Aide</a></button>
+<button><a class="a2">A Propos</a></button>
+
+</header>
+
+<body>
+
+</body>
+</html>
